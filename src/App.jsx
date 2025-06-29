8e379b98321e1fdcb1e754910e2afa8a8ca9dc3b
Harshit Saxena <harshitsaxena@MacBook-Pro-5.local>
2025-06-29 17:45:36 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { gsap } from "gsap/all";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 //this is needed to make the gsap be refistered globally in the system
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -10,6 +11,8 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero/>
+      <div className="h-dvh bg-black"/>
     </main>
   );
 };
